182b5628050cd9f70aa522edbd83a102882f7d0d
kachnazkoci <katka.kratkaa@gmail.com>
2022-12-06 17:24:46 +0100

    day4 edit

--- 20221204.py
+++ 20221204.py
@@ -64,8 +64,8 @@ import sys
 infile = sys.argv[1] if len(sys.argv)>1 else '4.in'
 data = open(infile).read().strip()
 lines = [x.strip() for x in data.split('\n')]
-p1 = 0
-p2 = 0
+ap1 = 0
+ap2 = 0
 for line in lines:
     one,two = line.split(',')
     s1,e1= one.split('-')
@@ -73,11 +73,11 @@ for line in lines:
     s1,e1,s2,e2 = [int(x) for x in [s1,e1,s2,e2]]
     # (s2,e2) is fully contained in (sl,el) if sl<=s2 and e2<=el
     if s1<=s2 and e2<=e1 or s2<=s1 and e1<=e2:
-        p1 += 1
+        ap1 += 1
     # (s2,e2) overlaps (sl,el) if it is not completely to the left or completely to the right
     #          s2 -- e2
     #       e1           s1
     if not (e1 < s2 or s1 > e2):
-        p2 += 1
-print(p1)
-print(p2)
+        ap2 += 1
+print(ap1)
+print(ap2)
