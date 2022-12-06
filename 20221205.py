e3104f892497c065e6bef0de8ea90b353ec6cbab
kachnazkoci <katka.kratkaa@gmail.com>
2022-12-06 17:27:17 +0100

    day5 edit

--- 20221205.py
+++ 20221205.py
@@ -1,3 +1,105 @@
+"""
+--- Day 5: Supply Stacks ---
+The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, but because the needed supplies are buried under many other crates, the crates need to be rearranged.
+
+The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
+
+The Elves don't want to interrupt the crane operator during this delicate procedure, but they forgot to ask her which crate will end up where, and they want to be ready to unload them as soon as possible so they can embark.
+
+They do, however, have a drawing of the starting stacks of crates and the rearrangement procedure (your puzzle input). For example:
+
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+In this example, there are three stacks of crates. Stack 1 contains two crates: crate Z is on the bottom, and crate N is on top. Stack 2 contains three crates; from bottom to top, they are crates M, C, and D. Finally, stack 3 contains a single crate, P.
+
+Then, the rearrangement procedure is given. In each step of the procedure, a quantity of crates is moved from one stack to a different stack. In the first step of the above rearrangement procedure, one crate is moved from stack 2 to stack 1, resulting in this configuration:
+
+[D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+In the second step, three crates are moved from stack 1 to stack 3. Crates are moved one at a time, so the first crate to be moved (D) ends up below the second and third crates:
+
+        [Z]
+        [N]
+    [C] [D]
+    [M] [P]
+ 1   2   3
+Then, both crates are moved from stack 2 to stack 1. Again, because crates are moved one at a time, crate C ends up below crate M:
+
+        [Z]
+        [N]
+[M]     [D]
+[C]     [P]
+ 1   2   3
+Finally, one crate is moved from stack 1 to stack 2:
+
+        [Z]
+        [N]
+        [D]
+[C] [M] [P]
+ 1   2   3
+The Elves just need to know which crate will end up on top of each stack; in this example, the top crates are C in stack 1, M in stack 2, and Z in stack 3, so you should combine these together and give the Elves the message CMZ.
+
+After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+Your puzzle answer was TQRFCBSJJ.
+
+--- Part Two ---
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+Your puzzle answer was RMHFJNVFP.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+"""
 import re
 import copy
 import itertools
@@ -66,125 +168,3 @@ with open("5.in", "r", encoding="UTF-8") as str_file:
     print(newTopCrates)
 
 
-
-"""
-
-use itertools::Itertools;
-
-#[derive(Debug)]
-struct Instruction {
-    amount: usize,
-    from: usize,
-    to: usize,
-}
-
-fn parse_input() -> Option<(Vec<Vec<char>>, Vec<Instruction>)> {
-    let input = std::fs::read_to_string("src/day05.txt").ok()?;
-    let (left, instructions_str) = input.split_once("\n\n")?;
-    let (stacks_str, platforms) = left.rsplit_once('\n')?;
-
-    // parse crates
-    let num_stacks = platforms.split_whitespace().last()?.parse().ok()?;
-    let mut stacks = vec![Vec::new(); num_stacks];
-
-    for line in stacks_str.lines().rev() {
-        for (idx, mut chunk) in line.chars().chunks(4).into_iter().enumerate() {
-            let second = chunk.nth(1)?;
-            if second.is_alphabetic() {
-                stacks[idx].push(second);
-            }
-        }
-    }
-
-    // parse instructions
-    let mut instructions = Vec::new();
-    for line in instructions_str.lines() {
-        let rest = line.strip_prefix("move ")?;
-        let (amount, rest) = rest.split_once(" from ")?;
-        let (from, to) = rest.split_once(" to ")?;
-        let instruction = Instruction {
-            amount: amount.parse().ok()?,
-            from: from.parse::<usize>().ok()? - 1,
-            to: to.parse::<usize>().ok()? - 1,
-        };
-        instructions.push(instruction);
-    }
-
-    Some((stacks, instructions))
-}
-
-pub fn part_1() -> String {
-    let (mut stacks, instructions) = parse_input().unwrap();
-    for Instruction { amount, from, to } in instructions {
-        for _ in 0..amount {
-            if let Some(removed) = stacks[from].pop() {
-                stacks[to].push(removed);
-            }
-        }
-    }
-
-    stacks
-        .iter()
-        .filter_map(|stack| stack.iter().last())
-        .collect()
-}
-
-pub fn part_2() -> String {
-    let (mut stacks, instructions) = parse_input().unwrap();
-    for Instruction { amount, from, to } in instructions {
-        let from_stack_len = stacks[from].len();
-        let removed = stacks[from].split_off(from_stack_len - amount);
-        stacks[to].extend(removed);
-    }
-
-    stacks
-        .iter()
-        .filter_map(|stack| stack.iter().last())
-        .collect()
-}
-"""
-"""
-
-import sys
-
-from copy import deepcopy
-infile = sys.argv[1] if len(sys.argv)>1 else '5.in'
-data = open(infile).read() #.strip()
-lines = [x for x in data.split('\n')]
-
-S = []
-cmds = []
-for line in lines:
-    print(line, len(line))
-    if line =='':
-        break
-    sz = (len(line)+1)//4
-    while len(S) < sz:
-        S.append([])
-    for i in range(len(S)):
-        ch = line[1 + 4*i]
-        if ch != ' ' and 'A'<=ch<='Z':
-            S[i].append(ch)
-print(S)
-
-S1 = deepcopy(S)
-S2 = deepcopy(S)
-found = False
-for cmd in lines:
-    if cmd == '':
-        found = True
-        continue
-    if not found:
-        continue
-    words = cmd.split()
-    qty = int(words[1])
-    from_ = int(words[3]) - 1
-    to_ = int(words[5]) - 1
-    for (ST, do_rev) in [(S1, True), (S2, False)]:
-        MOVE = ST[from_][:qty]
-        ST[from_] = ST[from_][:qty]
-        ST[to_] = (list(reversed(MOVE)) if do_rev else MOVE) + ST[to_]
-print(''.join([s[0] for s in S1 if len(s)>0]))
-print(''.join([s[0] for s in S2 if len(s)>0]))
-
-"""
